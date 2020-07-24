a95cc7b55735267debd0ec1ed9ff0c747d90d836
NataliaBondarenko <write_up@mail.ua>
2020-07-24 21:39:42 +0300

    added new functions for file preview, updated def generic_text_preview with utf-8 encoding

--- count_files/utils/file_handlers.py
+++ count_files/utils/file_handlers.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python3
 # encoding: utf-8
 import os
+import sys
+import subprocess
 from itertools import chain
 from typing import List, Tuple, Dict
 
-from count_files.settings import SUPPORTED_TYPES
+from count_files.settings import SUPPORTED_TYPES, BUG_REPORT_URL
 
 
 def get_file_extension(filepath: str, case_sensitive: bool = False) -> str:
@@ -71,3 +73,67 @@ def group_ext_by_type(data: List[Tuple[str, int]],
     if other:
         storage.update({'other': other})
     return storage
+
+
+def is_file_utility_available(file_path: str, expected_result: str) -> [bool, str]:
+    """Check if the Unix "file" command is available and works as expected.
+
+    The "file" command is a standard program on Unix and Unix-like OS
+    for guessing the type of data contained in a file.
+
+    This program can be used on Windows.
+    For example, if the user installed it along with Git (https://git-scm.com)
+    and added it to the PATH environment variable.
+    It can usually be located as C:\\Program Files\\Git\\usr\\bin\\file.exe
+
+    :param file_path: test path must exist
+    :param expected_result: expected standard output of Unix "file" command for a path
+    :return: True, None or False, error message
+    """
+    try:
+        file_help_text = subprocess.run(
+            ['file', '--help'], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
+        if 'Determine type of FILEs.' not in file_help_text:
+            msg = '\nAn error occurred while checking the "file" command.\n' \
+                  'Calling the "file" command with the "--help" option returned unexpected results.\n' \
+                  'Make sure you are applying the correct "file" program to your files.\n'
+            return False, msg
+        output = subprocess.run(
+            ['file', file_path], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
+        if output == expected_result:
+            return True, None
+        else:
+            msg = '\nAn error occurred while checking the "file" command.\n' \
+                  'Applying the "file" command to the test path returned unexpected results.\n' \
+                  'Make sure you are applying the correct "file" program to your files.\n'
+            return False, msg
+    except Exception as e:
+        # most common exception raised is OSError, FileNotFoundError: [Errno 2] No such file or directory: ...
+        # or  CalledProcessError: Command ... returned non-zero exit status 1 (unknown option, etc.)
+        msg = '\nAn error occurred while checking the "file" command.\n' \
+              f'{e.__class__.__name__}: {e}\n' \
+              'Make sure you are applying the correct "file" program to your files.\n' \
+              f'You can also report a problem at {BUG_REPORT_URL}\n'
+        return False, msg
+
+
+def check_shell_command() -> [bool, str]:
+    """Checking if we can use the Unix "file" command
+    to determine the file type for the preview.
+
+    In CLI, the path to the "count_files/utils/test" directory is used
+    to check the availability of the "file" program.
+
+    :return: True, None or False, error message
+    if False use default list of text extensions.
+    """
+    if not any([sys.platform.startswith('win'),
+                sys.platform.startswith('linux'),
+                sys.platform.startswith('darwin')]):
+        msg = '\nThe use of "-sc file" or "--shell-command file" on your operating system ' \
+              'is not yet supported in the Count Files application.\n'
+        return False, msg
+    else:
+        directory = os.path.normpath(os.path.join(os.path.dirname(__file__), 'test'))
+        return is_file_utility_available(
+            file_path=directory, expected_result=f'{directory}: directory\n')
--- count_files/utils/file_preview.py
+++ count_files/utils/file_preview.py
@@ -1,5 +1,8 @@
 #!/usr/bin/env python3
 # encoding: utf-8
+import mimetypes
+import locale
+import subprocess
 from pathlib import Path
 
 from count_files.utils.file_handlers import get_file_extension
@@ -9,16 +12,33 @@ from count_files.settings import SUPPORTED_TYPES
 def generic_text_preview(filepath: str, max_size: int) -> str:
     """Read the first characters of the file and return a string.
 
+    First try to open the file with encoding='utf-8',
+    if that fails (UnicodeDecodeError),
+    then with the user's preferred encoding.
+
     :param filepath: a string containing the path to the file
     :param max_size: max number of characters to be read from file
     :return: a string with the text preview or error message
     """
     try:
-        with open(filepath, mode='r') as f:
+        # try one of the most commonly used encodings
+        # a string of ASCII text is also valid UTF-8 text
+        with open(filepath, mode='r', encoding='utf-8') as f:
             return f.read(max_size).replace('\n', ' ')
     except Exception as e:
-        # DEBUG OSError
-        return f"TEXT_PREVIEW_ERROR: {e}"
+        if e.__class__ is UnicodeDecodeError:
+            # try encoding used for text data, according to user preferences
+            user_preferred_encoding = locale.getpreferredencoding(False)
+            if user_preferred_encoding == 'UTF-8':
+                return f"TEXT_PREVIEW_ERROR: {e.__class__.__name__}: {e}"
+            else:
+                try:
+                    with open(filepath, mode='r', encoding=user_preferred_encoding) as f:
+                        return f.read(max_size).replace('\n', ' ')
+                except Exception as err:
+                    return f"TEXT_PREVIEW_ERROR: {err.__class__.__name__}: {err}"
+        else:
+            return f"TEXT_PREVIEW_ERROR: {e.__class__.__name__}: {e}"
 
 
 # TODO: build a better preview system for binaries
@@ -39,7 +59,10 @@ def generic_binary_preview(filepath: str, max_size: int) -> bytes or str:
 
 
 def generate_preview(filepath: str, max_size: int = 390) -> str:
-    """Generate a human readable text preview.
+    """Generate a human readable text preview
+    after checking if the extension is in the list of supported types.
+
+    This function is used for all operating systems.
 
     For text files, the preview will be the first `max_size` characters.
     For other file types the preview is not implemented.
@@ -63,3 +86,47 @@ def generate_preview(filepath: str, max_size: int = 390) -> str:
     else:
         # skip the extension if it is not supported
         return "[A preview of this file type is not yet implemented.]"
+
+
+def generate_preview_with_file(filepath: str, max_size: int = 390) -> str:
+    """Generate a human readable text preview
+    after checking the file type with the Unix "file" command.
+
+    This function is used in Linux, Mac OS and Windows
+    (if the "file" program is installed
+    and added to the PATH environment variable there).
+
+    For text files, the preview will be the first `max_size` characters.
+    For other file types the preview is not implemented.
+    :param filepath: full/path/to/file (with extension or without it)
+    :param max_size:
+    For CLI.
+    The number of characters for viewing by default depends on the terminal width settings
+    and can be changed with the -ps or -preview-size argument.
+    :return: a string with the text preview (without newline characters).
+    If the preview is not available for the file, it returns an information message.
+    """
+    try:
+        process_output = subprocess.run(
+            ['file', filepath], check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).stdout.decode('utf-8')
+        # filepath: message\n
+        message = process_output.split(': ')[1].strip('\n')
+    except Exception as e:
+        # OSError or CalledProcessError
+        return f"TEXT_PREVIEW_ERROR: {e.__class__.__name__}: {e}"
+    if 'cannot open' in message:
+        # No such file or directory, or cannot read regular file
+        return f"TEXT_PREVIEW_ERROR: {message}"
+    elif 'empty' in message:
+        return "[This file is empty.]"
+    elif 'text' in message:
+        excerpt = generic_text_preview(filepath, max_size)
+        if excerpt:
+            # return excerpt or error string
+            return f"{excerpt}"
+        else:
+            return "[This file can be empty.]"
+    else:
+        # skip the extension if it is not supported
+        # also: very short file (no magic)
+        return f"[A preview of this file type is not yet implemented: {message}.]"
